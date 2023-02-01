d289fd8ac769acbd1c6478cb560144392f2ac19a
Vladislav Manakov <vsmanakov@avito.ru>
2023-02-01 08:29:25 +0700

    Добавляет базовое мобильное меню

---
+++ src/components/mobile-menu/index.js
@@ -0,0 +1,48 @@
+export class MobileMenu {
+  constructor() {
+    this.button = document.querySelector('#js-mobile-menu-button');
+    this.menu = document.querySelector('#js-mobile-menu');
+    this.isOpen = false;
+
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.button.addEventListener('click', () => this.toggle());
+  }
+
+  toggle() {
+    if (this.isOpen) {
+      this.close();
+    } else {
+      this.open();
+    }
+
+    this.button.classList.toggle('is-open');
+    this.isOpen = !this.isOpen;
+  }
+
+  open() {
+    this.show();
+
+    setTimeout(() => {
+      this.animate();
+    }, 0);
+  }
+
+  close() {
+    this.animate();
+
+    setTimeout(() => {
+      this.show();
+    }, 300);
+  }
+
+  show() {
+    this.menu.classList.toggle('is-show');
+  }
+
+  animate() {
+    this.menu.classList.toggle('is-animate');
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './styles/index.less';
-import { Dropdown } from './components/dropdown'
-import { UserWidget } from './components/user-widget'
+import { Dropdown } from './components/dropdown';
+import { UserWidget } from './components/user-widget';
+import { MobileMenu } from './components/mobile-menu';
 
 document.querySelectorAll('.dropdown').forEach((dropdown) => {
-    new Dropdown(dropdown);
+  new Dropdown(dropdown);
 });
 
 new UserWidget();
+new MobileMenu();
